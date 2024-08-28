bc88fc1c3910c64f33ee63f5c69e07f6fa813327
Quinn <getreadytogetkaidfiyahhhh@gmail.com>
2024-08-28 11:18:48 +0200

    uppgift 1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hej! Vad är ditt förnamn??");
+string förnamn= Console.ReadLine();
+
+Console.WriteLine("coolt namn du har " + förnamn +"! Men vad är ditt efternamn??");
+string efternamn= Console.ReadLine();
+Console.WriteLine("det blev ju ännu coolare! så du heter " + förnamn + efternamn + " :)");
+
+System.Console.WriteLine("Testa mig med lite matte! Ge mig ett heltal.");
